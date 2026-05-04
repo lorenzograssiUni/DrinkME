5f1971bcfd6fd6f19ddf2742b07f558e480e7a63
lorenzograssiUni <lorenzo.grassi@studenti.unicam.it>
2026-05-04 18:08:55 +0200

    fix: UominiAnimation - cerchio, personaggio davanti al mare, nuvoletta corretta

--- src/animations/UominiAnimation.jsx
+++ src/animations/UominiAnimation.jsx
@@ -3,13 +3,13 @@ import "./UominiAnimation.css";
 export default function UominiAnimation({ onClose }) {
     return (
         <div className="uomini-overlay" onClick={onClose}>
-            <p className="uomini-titolo">BEVONO I MASCHI!</p>
-
-            <div className="uomini-scene">
-                {/* Cielo parallax */}
+            <div className="uomini-circle">
+                {/* Sfondo cielo parallax */}
                 <div className="uomini-sky" />
+                {/* Mare parallax */}
+                <div className="uomini-sea" />
 
-                {/* Personaggio + nuvoletta */}
+                {/* Personaggio + nuvoletta - sopra tutto */}
                 <div className="uomini-char-wrap">
                     <div className="uomini-thought">
                         <span className="uomini-thought-emoji">🍺</span>
@@ -17,13 +17,11 @@ export default function UominiAnimation({ onClose }) {
                         <div className="uomini-thought-dot d2" />
                         <div className="uomini-thought-dot d3" />
                     </div>
-                    <div id="uomini-bern" />
+                    <div className="uomini-bern" />
                 </div>
-
-                {/* Mare parallax */}
-                <div className="uomini-sea" />
             </div>
 
+            <p className="uomini-titolo">BEVONO I MASCHI!</p>
             <p className="uomini-sottotitolo">Tutti gli uomini devono bere! 🍺</p>
             <span className="uomini-hint">Tocca per continuare</span>
         </div>
