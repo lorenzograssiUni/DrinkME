9409be29b2312a5692b67be669a3dda6b1dda2d7
lorenzograssiUni <lorenzo.grassi@studenti.unicam.it>
2026-05-04 18:15:09 +0200

    feat: UominiAnimation - baffi CSS animati

--- src/animations/UominiAnimation.jsx
+++ src/animations/UominiAnimation.jsx
@@ -4,20 +4,31 @@ export default function UominiAnimation({ onClose }) {
     return (
         <div className="uomini-overlay" onClick={onClose}>
             <div className="uomini-circle">
-                {/* Sfondo cielo parallax */}
-                <div className="uomini-sky" />
-                {/* Mare parallax */}
-                <div className="uomini-sea" />
+                {/* Emoji sfondo */}
+                <span className="uomini-bg e1">💪</span>
+                <span className="uomini-bg e2">⚡</span>
+                <span className="uomini-bg e3">🔥</span>
+                <span className="uomini-bg e4">💪</span>
+                <span className="uomini-bg e5">⚡</span>
+                <span className="uomini-bg e6">🔥</span>
+                <span className="uomini-bg e7">💪</span>
+                <span className="uomini-bg e8">⚡</span>
+                <span className="uomini-bg e9">🔥</span>
+                <span className="uomini-bg e10">💪</span>
+                <span className="uomini-bg e11">⚡</span>
+                <span className="uomini-bg e12">🔥</span>
 
-                {/* Personaggio + nuvoletta - sopra tutto */}
-                <div className="uomini-char-wrap">
-                    <div className="uomini-thought">
-                        <span className="uomini-thought-emoji">🍺</span>
-                        <div className="uomini-thought-dot d1" />
-                        <div className="uomini-thought-dot d2" />
-                        <div className="uomini-thought-dot d3" />
+                {/* Faccia con baffi */}
+                <div className="uomini-face">
+                    <div className="uomini-eyes">
+                        <div className="uomini-eye left" />
+                        <div className="uomini-eye right" />
                     </div>
-                    <div className="uomini-bern" />
+                    <div className="uomini-nose" />
+                    <div className="uomini-mustache">
+                        <div className="uomini-must-left" />
+                        <div className="uomini-must-right" />
+                    </div>
                 </div>
             </div>
 
