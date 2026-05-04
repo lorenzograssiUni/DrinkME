3eff1b0d4dabf9b5cd8a3724b84d0e0870ea6e73
lorenzograssiUni <lorenzo.grassi@studenti.unicam.it>
2026-05-04 18:05:17 +0200

    feat: UominiAnimation - personaggio sprite animato con nuvoletta pensiero birra

--- src/animations/UominiAnimation.jsx
+++ src/animations/UominiAnimation.jsx
@@ -3,35 +3,27 @@ import "./UominiAnimation.css";
 export default function UominiAnimation({ onClose }) {
     return (
         <div className="uomini-overlay" onClick={onClose}>
-            <div className="uomini-circle">
-                {/* Emoji di sfondo */}
-                <span className="uomini-bg e1">💪</span>
-                <span className="uomini-bg e2">⚡</span>
-                <span className="uomini-bg e3">👏</span>
-                <span className="uomini-bg e4">🔥</span>
-                <span className="uomini-bg e5">💪</span>
-                <span className="uomini-bg e6">⚡</span>
-                <span className="uomini-bg e7">🌟</span>
-                <span className="uomini-bg e8">🔥</span>
-                <span className="uomini-bg e9">🌟</span>
-                <span className="uomini-bg e10">👏</span>
-                <span className="uomini-bg e11">⚡</span>
-                <span className="uomini-bg e12">💪</span>
+            <p className="uomini-titolo">BEVONO I MASCHI!</p>
 
-                {/* Boccale birra */}
-                <div className="uomini-beer">
-                    <div className="uomini-beer-body">
-                        <div className="uomini-beer-foam" />
-                        <div className="uomini-beer-liquid" />
-                        <div className="uomini-beer-bubble b1" />
-                        <div className="uomini-beer-bubble b2" />
-                        <div className="uomini-beer-bubble b3" />
+            <div className="uomini-scene">
+                {/* Cielo parallax */}
+                <div className="uomini-sky" />
+
+                {/* Personaggio + nuvoletta */}
+                <div className="uomini-char-wrap">
+                    <div className="uomini-thought">
+                        <span className="uomini-thought-emoji">🍺</span>
+                        <div className="uomini-thought-dot d1" />
+                        <div className="uomini-thought-dot d2" />
+                        <div className="uomini-thought-dot d3" />
                     </div>
-                    <div className="uomini-beer-handle" />
+                    <div id="uomini-bern" />
                 </div>
+
+                {/* Mare parallax */}
+                <div className="uomini-sea" />
             </div>
 
-            <p className="uomini-titolo">BEVONO I MASCHI!</p>
             <p className="uomini-sottotitolo">Tutti gli uomini devono bere! 🍺</p>
             <span className="uomini-hint">Tocca per continuare</span>
         </div>
